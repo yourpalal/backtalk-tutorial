539b196605ff8f62f32812497462e1df2e489d65
Alex Wilson <aewilson@ualberta.ca>
2015-12-29 18:28:08 -0700

    use path.basename instead of file.basename in linkto

--- squick_extensions.js
+++ squick_extensions.js
@@ -1,12 +1,12 @@
-var gutil = require("gulp-util");
+var gutil = require("gulp-util"),
+    path = require("path");
 
 module.exports.filters = {};
 
 module.exports.helpers = {
     linkto: function(chunk, context, bodies, params) {
         var post = params.post || context.current();
-        var ext;
-        if ("index.md" === post.file.basename) {
+        if ("index.md" === path.basename(post.file.path)) {
             ext = ".html"; // just link the HTML output
         } else {
             ext = "/"; // link file/ so that it becomes file/index.html
